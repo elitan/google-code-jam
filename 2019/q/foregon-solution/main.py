e7415eb8af27f47f9995e6794ce6f4f8e01b9502
elitan <johan@eliasson.me>
2019-04-06 14:15:19 +0200

    q first problem done

---
+++ 2019/q/foregon-solution/main.py
@@ -0,0 +1,21 @@
+import fileinput
+
+lines = int(input())
+
+for x in range(lines):
+    n_str = input()
+
+    a = ''
+    b = ''
+    for i, l in enumerate(n_str):
+        if l == '4':
+            b += '3'
+            a += '1'
+        else:
+            b += l
+            a += '0'
+
+    a = int(a)
+    b = int(b)
+    case = x + 1
+    print('Case #{}: {} {}'.format(case, a, b))
